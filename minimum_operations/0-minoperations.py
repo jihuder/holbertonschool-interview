80764205950444cd4c370aed6ed113538847f01e
jihuder <julioarenas85@gmail.com>
2023-04-05 18:40:06 -0500

    fix: correccion de pruebas

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -15,14 +15,11 @@ def minOperations(n):
     factors = []
     while n % 2 == 0:
         factors.append(2)
-        print(factors)
         n //= 2
 
     for i in range(3, int(math.sqrt(n)) + 1, 2):
-        print("soy el ciclo for")
         while n % i == 0:
             factors.append(i)
-            print(factors)
             n //= i
 
     if n > 2:
